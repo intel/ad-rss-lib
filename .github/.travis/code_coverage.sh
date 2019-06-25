7634fd6f040e33e176d5be83638a74b29f5a012b
Fabian Oboril <fabian.oboril@intel.com>
2019-06-25 09:32:45 +0200

    Added integration for CodeCov

--- .github/.travis/code_coverage.sh
+++ .github/.travis/code_coverage.sh
@@ -39,4 +39,5 @@ lcov -q -a initialCov.info -a testCov.info -o coverage.info --rc lcov_branch_cov
 lcov -q -r coverage.info "*test*" -o coverage.info --rc lcov_branch_coverage=1
 lcov --summary coverage.info --rc lcov_branch_coverage=1
 genhtml -q -t ad-rss-lib --branch-coverage -o coverage coverage.info
+bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
 
