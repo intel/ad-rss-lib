6cf590d4fbd379dce36f26de07399a6ecd73d404
Fabian Oboril <fabian.oboril@intel.com>
2020-03-06 10:11:57 +0100

    Further Travis fixes for documentation deployment

--- .github/.travis/deploy.sh
+++ .github/.travis/deploy.sh
@@ -9,26 +9,25 @@
 # ----------------- END LICENSE BLOCK -----------------------------------
 ##
 
-git checkout -b intermediate
-mkdir gh-pages
-mkdir -p gh-pages/doxygen/ad_rss/
-mkdir -p gh-pages/doxygen/ad_rss_map_integration/
-mkdir gh-pages/documentation
-mkdir gh-pages/coverage
-cp -r coverage/* gh-pages/coverage/
-cp -r build/ad_rss/apidoc/html/* gh-pages/doxygen/ad_rss/
-cp -r build/ad_rss_map_integration/apidoc/html/* gh-pages/doxygen/ad_rss_map_integration/
-cp -r site/* gh-pages/
-git add gh-pages/
-git commit -m "Dummy commit" --no-edit
-git clean -fd
-git checkout origin/gh-pages
-git checkout -b gh-pages
-git checkout intermediate gh-pages/
-cp -r gh-pages/* .
-rm -rf gh-pages/ build/
-git add .
+git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/intel/ad-rss-lib.git gh-pages > /dev/null 2>&1
+cd gh-pages
+
+mkdir -p doxygen/ad_rss/
+mkdir -p doxygen/ad_rss_map_integration/
+mkdir coverage
+cp -r ../coverage/* coverage/.
+cp -r ../build/ad_rss/apidoc/html/* doxygen/ad_rss/.
+cp -r ../build/ad_rss_map_integration/apidoc/html/* doxygen/ad_rss_map_integration/.
+cp -r ../site/* .
+
+git add -f .
+git status
+
 git commit -am "Update documentation" --no-edit
 git remote add origin-pages https://${GITHUB_TOKEN}@github.com/intel/ad-rss-lib.git > /dev/null 2>&1
-git push --set-upstream origin-pages gh-pages
 
+if [ "$1" = true ]; then
+  git push --set-upstream origin-pages gh-pages
+else
+  echo "Only testing mode - No deployment"
+fi
