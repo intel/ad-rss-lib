209877092be6c602c034a596a86c8f22354b335e
Bernd Gassmann <bernd.gassmann@intel.com>
2021-11-24 12:17:29 +0100

    Release 4.5.1

--- cmake/ad-rss-lib-version.cmake
+++ cmake/ad-rss-lib-version.cmake
@@ -6,11 +6,11 @@
 
 set(AD-RSS-LIB_VERSION_MAJOR 4)
 set(AD-RSS-LIB_VERSION_MINOR 5)
-set(AD-RSS-LIB_VERSION_REVISION 0)
-set(AD-RSS-LIB_VERSION 4.5.0)
+set(AD-RSS-LIB_VERSION_REVISION 1)
+set(AD-RSS-LIB_VERSION 4.5.1)
 
 if (EXISTS ${CURRENT_LISTS_DIR}/../dependencies/map/cmake/carla-map-version.cmake)
   include(${CURRENT_LISTS_DIR}/../dependencies/map/cmake/carla-map-version.cmake)
 else()
-  set(CARLA-MAP_VERSION 2.5.0)
+  set(CARLA-MAP_VERSION 2.6.0)
 endif()
