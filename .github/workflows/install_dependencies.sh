c346acbfe3b7445bed2f521c310e617c0607e454
Fabian Oboril <fabian.oboril@intel.com>
2023-11-11 14:06:16 +0100

    Update install_dependencies.sh

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends build-essential castxml cmake libboost-all-dev libgtest-dev liblapacke-dev libopenblas-dev libproj-dev libpugixml-dev libpython3-dev python python-setuptools python3 python3-pip python3-setuptools python3-wheel
 
-if [ `lsb_release -a | grep Release | grep 20.04 | wc -l` == 1 ]; then
+if [ `lsb_release -a | grep Release | grep 2*.04 | wc -l` == 1 ]; then
   sudo apt-get install -y --no-install-recommends python-is-python3
 else
   sudo apt-get install -y --no-install-recommends python-pip python-wheel
