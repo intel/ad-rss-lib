23985c216f1e43856acf48d18ad284c0e7185bac
Bernd Gassmann <bernd.gassmann@intel.com>
2024-01-25 16:26:56 +0100

    Adding 3.8 python for 22.04

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -4,6 +4,10 @@ python_native_pkgs="libpython3-dev python3 python3-pip python3-setuptools python
 python3_8_pkgs="libpython3.8-dev python3.8 python3.8-pip python3.8-setuptools python3.8-wheel"
 other_pkgs="build-essential castxml cmake libboost-all-dev libgtest-dev liblapacke-dev libopenblas-dev libpugixml-dev sqlite3"
 
+if [ `lsb_release -a | grep Release | grep 22.04 | wc -l` == 1 ]; then
+  sudo echo "deb https://ppa.launchpadcontent.net/ubuntu-toolchain-r/test/ubuntu/ jammy main" >> /etc/apt/sources.list.d/ubuntu-toolchain-r-ubuntu-test-jammy.list
+fi
+
 sudo apt-get update
 sudo apt-get install -y lsb-core
 sudo apt-get install -y --no-install-recommends ${other_pkgs} ${python3_pkgs} ${python3_8_pkgs}
