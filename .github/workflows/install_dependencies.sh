bdc2162a8b3e5c273c0607a48c66e69aec99590e
Bernd Gassmann <bernd.gassmann@intel.com>
2024-01-25 15:59:03 +0100

    Remove 18.04 support

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-python3_pkgs="libpython3-dev python3 python3-pip python3-setuptools python3-wheel"
-python2_pkgs="libpython-dev python python-pip python-wheel python-setuptools"
+python_native_pkgs="libpython3-dev python3 python3-pip python3-setuptools python3-wheel"
+python3_8_pkgs="libpython3.8-dev python3.8 python3.8-pip python3.8-setuptools python3.8-wheel"
 other_pkgs="build-essential castxml cmake libboost-all-dev libgtest-dev liblapacke-dev libopenblas-dev libpugixml-dev sqlite3"
 
 sudo apt-get update
 sudo apt-get install -y lsb-core
+sudo apt-get install -y --no-install-recommends ${other_pkgs} ${python3_pkgs} ${python3_8_pkgs}
 
-if [ `lsb_release -a | grep Release | grep 18.04 | wc -l` == 1 ]; then
-  sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-fi
-
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends ${other_pkgs} ${python3_pkgs}
-
-sudo pip3 install --upgrade setuptools==51.1.2
 sudo pip3 install colcon-common-extensions xmlrunner pygccxml pyplusplus
 
-if [ `lsb_release -a | grep Release | grep 18.04 | wc -l` == 1 ]; then
-  sudo apt-get install -y --no-install-recommends ${python2_pkgs}
-  sudo pip2 install --upgrade setuptools==41.1.0
-  sudo pip2 install pygccxml==2.2.1 pyplusplus==1.8.4 xmlrunner
-else
+if [ `lsb_release -a | grep Release | grep 20.04 | wc -l` == 1 ]; then
+  sudo pip3 install --upgrade setuptools==51.1.2
   sudo apt-get install -y --no-install-recommends "python-is-python3"
+else
+  sudo pip3.8 install --upgrade setuptools==59.6.0
+  sudo pip3 install --upgrade setuptools==59.6.0
 fi
-
