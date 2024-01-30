236f8fbaa0408a871ae1e4c9ee1d8bc95e7ab80d
Bernd Gassmann <bernd.gassmann@intel.com>
2024-01-30 11:08:24 +0100

    finally?

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -9,12 +9,12 @@ sudo apt-get install -y --no-install-recommends build-essential castxml cmake li
 
 COMPILE_BOOST=0
 
+
 if [ "${BOOST_VERSION}" != "" -a `lsb_release -a | grep Release | grep 20.04 | wc -l` == 1 -a "${PYTHON_BINDING_VERSION}" == "3.10" ]; then
   sudo add-apt-repository ppa:deadsnakes/ppa -y
   sudo apt-get update
   COMPILE_BOOST=1
   sudo apt-get install -y --no-install-recommends python${PYTHON_BINDING_VERSION}-full
-  sudo apt autoremove python3.8 -y
 else
   sudo apt-get install -y --no-install-recommends python${PYTHON_BINDING_VERSION}
   sudo apt-get install -y --no-install-recommends libboost-all-dev
@@ -24,8 +24,11 @@ sudo apt-get install -y --no-install-recommends python-is-python3 python${PYTHON
 curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python${PYTHON_BINDING_VERSION}
 #sudo python${PYTHON_BINDING_VERSION} -m pip install --upgrade pip
 sudo pip${PYTHON_BINDING_VERSION} install --upgrade setuptools==59.6.0
-sudo pip${PYTHON_BINDING_VERSION} install python-wheel
 sudo pip${PYTHON_BINDING_VERSION} install colcon-common-extensions xmlrunner pygccxml pyplusplus
+
+if [ `lsb_release -a | grep Release | grep 20.04 | wc -l` == 1 ]; then
+  sudo apt autoremove python2 -y
+fi
 
 if (( COMPILE_BOOST )); then
   pushd dependencies
@@ -33,11 +36,11 @@ if (( COMPILE_BOOST )); then
   tar -xzf ${BOOST_PACKAGE}.tar.gz
   pushd ${BOOST_PACKAGE}
 
-  py3="/usr/bin/env python${PYTHON_BINDING_VERSION}"
+  py3=`which python3.10`
   py3_root=`${py3} -c "import sys; print(sys.prefix)"`
   pyv=`$py3 -c "import sys;x='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(x)";`
 
-  echo "  ./bootstrap.sh \
+  echo "$pyv  ./bootstrap.sh \
     --prefix=${ROOT_DIR}/install/boost
     --with-libraries=python,filesystem,system,program_options \
                   --with-python=${py3} --with-python-root=${py3_root}"
