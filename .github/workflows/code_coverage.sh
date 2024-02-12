bfa3d2a7e2fb0d1d5667f511c84b2911b8cbe731
Bernd Gassmann <bernd.gassmann@intel.com>
2024-02-12 13:17:37 +0100

    Try to fix Pinned Dependencies

--- .github/workflows/code_coverage.sh
+++ .github/workflows/code_coverage.sh
@@ -14,14 +14,14 @@ get_coverage() {
     BUILD_DIR=$1
     SRC_DIR=$2
     PACKAGE_NAME=$3
-    lcov -q -i -c -d ${BUILD_DIR} -b ${SRC_DIR} --no-external -o initialCoverage.info
-    lcov -q -c -d ${BUILD_DIR} -b ${SRC_DIR} --no-external -o testCoverage.info --rc lcov_branch_coverage=1
-    lcov -q -a initialCoverage.info -a testCoverage.info -o coverage.info --rc lcov_branch_coverage=1
-    lcov -q -r coverage.info "impl/test*" -o coverage.info --rc lcov_branch_coverage=1
-    grep -v -E 'D0Ev|D1Ev|C1Ev|C1Eb' coverage.info > cleanedCoverage.info
-    lcov --summary cleanedCoverage.info --rc lcov_branch_coverage=1
-    genhtml -t ${PACKAGE_NAME} -p ${PWD}/${SRC_DIR} --branch-coverage -o coverage/${SRC_DIR} cleanedCoverage.info
-    bash <(curl -s https://codecov.io/bash) -f cleanedCoverage.info || echo "Codecov did not collect coverage reports"
+    lcov -q -i -c -d ${BUILD_DIR} -b ${SRC_DIR} --no-external -o initialCoverage_${PACKAGE_NAME}.info
+    lcov -q -c -d ${BUILD_DIR} -b ${SRC_DIR} --no-external -o testCoverage_${PACKAGE_NAME}.info --rc lcov_branch_coverage=1
+    lcov -q -a initialCoverage_${PACKAGE_NAME}.info -a testCoverage_${PACKAGE_NAME}.info -o coverage_${PACKAGE_NAME}.info --rc lcov_branch_coverage=1
+    lcov -q -r coverage_${PACKAGE_NAME}.info "impl/test*" -o coverage_${PACKAGE_NAME}.info --rc lcov_branch_coverage=1
+    grep -v -E 'D0Ev|D1Ev|C1Ev|C1Eb' coverage_${PACKAGE_NAME}.info > cleanedCoverage_${PACKAGE_NAME}.info
+    lcov --summary cleanedCoverage_${PACKAGE_NAME}.info --rc lcov_branch_coverage=1
+    genhtml -t ${PACKAGE_NAME} -p ${PWD}/${SRC_DIR} --branch-coverage -o coverage/${SRC_DIR} cleanedCoverage_${PACKAGE_NAME}.info
+    bash <(curl -s https://codecov.io/bash) -f cleanedCoverage_${PACKAGE_NAME}.info || echo "Codecov did not collect coverage reports"
 }
 
 get_coverage build/ad_rss ad_rss ad_rss
